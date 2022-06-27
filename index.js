c56f67ccaf7d3fd18b8964f01defb0ff6e577c0c
Matteo <gnappo.1@live.it>
2022-06-27 20:32:20 +0200

    commit tree e6998882cf836b1fb9b064f74f9bfe5e7e1e0a12

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function findMatching(driversArray, nameToFind) {
+    return driversArray.filter(driver => driver.toLowerCase() === nameToFind.toLowerCase())
+    // const finalArray = [];
+    // for (const driver of driversArray) {
+    //     if (driver.toLowerCase() === nameToFind.toLowerCase()) { finalArray.push(driver) }
+    // }
+    // return finalArray
+}
