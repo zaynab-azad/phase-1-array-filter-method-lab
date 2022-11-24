c2c5ae6719932990f5830308cf64069e40e096dd
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-24 09:58:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(
+    (element) => element.toUpperCase() == string.toUpperCase()
+  );
+}
+function fuzzyMatch(drivers, string) {
+  return drivers.filter((element) =>
+    element.toUpperCase().startsWith(string.toUpperCase())
+  );
+}
+function matchName(drivers, string) {
+  return drivers.filter(
+    (element) => element.name.toUpperCase() == string.toUpperCase()
+  );
+}
